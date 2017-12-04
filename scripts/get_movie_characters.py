5b65bc35865a5156916b04da9bae159884d578e3
Reynier de la Rosa <sirio3mil@users.noreply.github.com>
2017-12-04 18:46:27 +0100

    changing to json

--- scripts/get_movie_characters.py
+++ scripts/get_movie_characters.py
@@ -10,28 +10,29 @@ tables = soup.find_all('table', {'class': 'cast_list'})
 cleanr = re.compile('<.*?>')
 
 for row in tables[0].find_all('tr'):
+    actor = {}
     td_tags = row.find_all('td')
     if len(td_tags) == 4:
         a = td_tags[1].find_all('a')[0]
-        imdbId = re.findall('\d+', a['href'])[0]
-        name = re.sub(cleanr, '', td_tags[1].__str__().strip())
-        name = " ".join(name.split())
+        actor['imdb'] = re.findall('\d+', a['href'])[0]
+        actor['name'] = re.sub(cleanr, '', td_tags[1].__str__().strip())
+        actor['name'] = " ".join(actor['name'].split())
         character = re.sub(cleanr, '', td_tags[3].__str__().strip())
         character = " ".join(character.split())
         pos = character.find('(')
         alternatives = []
         if pos > 0:
-            extras = re.findall('\(.*?\)', character)
+            actor['extras'] = re.findall('\(.*?\)', character)
             character = character[0:pos]
-            extras = list(map(lambda s: s[1:-1], extras))
-            for extra in extras:
+            actor['extras'] = list(map(lambda s: s[1:-1], actor['extras']))
+            for extra in actor['extras']:
                 if extra.startswith('as '):
-                    alternatives.append(extra.replace('as ', ''))
-                    extras.remove(extra)
-        else:
-            extras = []
+                    if 'alternatives' not in actor.keys():
+                        actor['alternatives'] = []
+                    actor['alternatives'].append(extra.replace('as ', ''))
+                    actor['extras'].remove(extra)
         pos = character.find(' / ')
         if pos > 0:
-            characters = character.split(' / ')
+            actor['characters'] = character.split(' / ')
         else:
-            characters = [character]
+            actor['characters'] = [character]
